0b193dea651653f7d842285535e754aab0933875
wexort <bugwriter@outlook.com>
2020-09-16 16:04:26 +0200

    semVer 0.1.0

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hughluo/go-mod-demo
+
+go 1.12
---
+++ semver.go
@@ -0,0 +1,7 @@
+package semver
+
+// SemVer return the semantic version string
+func SemVer() (semVer string) {
+	semVer = "0.1.0"
+	return
+}
